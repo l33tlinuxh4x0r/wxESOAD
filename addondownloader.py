8cd77e52b670b6d52166b2807ba94eb5c65f1449
l33tlinuxh4x0r <l33tlinuxh4x0r@gmail.com>
2023-06-19 13:10:12 -0400

    Initial commit
    Uploading the files that are needed to run/build wxESOAD.

---
+++ addondownloader.py
@@ -0,0 +1,72 @@
+import re, shutil, os, re, zipfile, time, ssl, wx
+from urllib.request import urlopen, Request
+
+class AddonDownloader():
+
+    file_number = 0
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    addons = ""
+    addons_location = ""
+    addon_temp_folder = "addontemp"
+    addon_temp_name = "addon{0}.zip"
+
+    #GUI functions
+    set_status_text = None
+
+    def __init__(self, func_set_status_text):
+        self.set_status_text = func_set_status_text
+
+    def start(self):
+        self.refresh_addon_location()
+        self.set_status_text("Starting....")
+        if os.path.isdir(self.addon_temp_folder) == False:
+            os.mkdir(self.addon_temp_folder)
+        links = self.addons.split("\n")
+        self.file_number = 0
+        for link in links:
+            if link != "" and link.startswith("#") != True:
+                info = re.findall("esoui.com/downloads/info(\d*)", link)[0]
+                download_url = f"https://cdn.esoui.com/downloads/file{info}/{str(int(time.time()))}"
+                file = self.download(self.file_number, download_url)
+                if file == False:
+                    print("Failed to use the new url, using old url instead...")
+                    download_url = f"https://cdn.esoui.com/downloads/file{info}/"
+                    file = self.download(self.file_number, download_url)
+                self.unzip(file)
+                self.file_number += 1
+        #delete temp folder
+        shutil.rmtree(self.addon_temp_folder)
+        self.end()
+
+    def download(self, file_number, download_url):
+        self.set_status_text("Downloading: " + download_url)
+        tempfilename = self.addon_temp_folder + "/" + self.addon_temp_name.format(str(file_number))
+        request = Request(url=download_url, headers=self.headers)
+        response = urlopen(request, cafile=None)
+        if response.getcode() != 200:
+            return False
+        with open(tempfilename, "wb") as f:
+            f.write(response.read())
+        return tempfilename
+
+
+    def unzip(self, file, custom_location=""):
+        self.set_status_text("Unzipping: " + file)
+        with zipfile.ZipFile(file, 'r') as z:
+            if custom_location == "":
+                z.extractall(self.addons_location)
+            else:
+                z.extractall(custom_location)
+
+    def refresh_addon_location(self):
+        addons_file = open("addons.txt", "r")
+        addons_location_file = open("addonslocation.txt", "r")
+        self.addons = addons_file.read()
+        self.addons_location = addons_location_file.read()
+
+    def end(self):
+        self.set_status_text("Done! Addons downloaded and unzipped.")
+        wx.MessageBox(f"Addons downloaded and unzipped to {self.addons_location}", "Success!")
+
---
+++ wxESOAD.py
@@ -0,0 +1,100 @@
+import os, platform, sys, wx
+from pathlib import Path
+from threading import Thread
+from addondownloader import AddonDownloader
+
+def touch_file(filename):
+    """Makes sure file exists"""
+    filename = Path(filename)
+    filename.touch(exist_ok=True)
+    return filename
+
+def create_addon_files():
+    addons_file = open(touch_file("addons.txt"), "r+")
+    addons_location_file = open(touch_file("addonslocation.txt"), "r+")
+    addons = addons_file.read()
+    addons_location = addons_location_file.read()
+    if addons_location == "":
+        if platform.system() == "Windows":
+            addons_location = f"{Path.home()}/Documents/Elder Scrolls Online/live/AddOns"
+        else:
+            addons_location = f"{Path.home()}/.local/share/Steam/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/Documents/Elder Scrolls Online/live/AddOns"
+    addons_file.close()
+    addons_location_file.close()
+    return addons_location, addons
+
+def update_status_text(text):
+    statusbar.SetStatusText(text)
+
+def on_start_download(event):
+    #Save all the input data to text files
+    #ESO addon location folder
+    addons_location_field.SaveFile(filename="addonslocation.txt")
+    #List of links
+    textbuffer = addon_link_textview.SaveFile(filename="addons.txt")
+
+    adlthread = Thread(target=adl.start)
+    handle_thread(adlthread)
+
+def handle_thread(thread):
+    thread.daemon = True
+    try:
+        thread.start()
+    except Exception as err:
+        update_status_text(str(err))
+
+
+addons_location, addons = create_addon_files()
+adl = AddonDownloader(update_status_text)
+
+# GUI starts here
+app = wx.App(False)
+frame = wx.Frame(None, wx.ID_ANY, title="wxESOAD (ESO Addon Downloader)", size=(500, 600))
+frame.SetIcon(wx.Icon(os.path.join(os.path.dirname(__file__), "esotux.png")))
+frame.Centre()
+frame.SetMinSize(size=(500, 600))
+
+p = wx.Panel(frame)
+vs = wx.BoxSizer(wx.VERTICAL)
+
+# Addons location
+addonslbl = wx.StaticText(p, wx.ID_ANY, label="ESO Addon folder location:")
+addons_location_field = wx.TextCtrl(p, value=addons_location)
+
+def select_directory(event):
+    with wx.DirDialog(None, "Select Addons Directory", defaultPath=addons_location_field.GetLineText(0), style=wx.DD_SHOW_HIDDEN) as addons_location:
+        addons_modal = addons_location.ShowModal()
+        if addons_modal != wx.ID_OK:
+            pass
+        else:
+            addons_location_field.SetValue(value=addons_location.GetPath())
+
+aobutton = wx.Button(p, wx.ID_ANY, "Select")
+aobutton.Bind(wx.EVT_BUTTON, select_directory)
+
+# Addons links
+addonslinkslbl = wx.StaticText(p, wx.ID_ANY, label="Links to ESOUI.com addon pages, one per line:")
+addon_link_textview = wx.TextCtrl(p, wx.ID_ANY, value=addons, style=wx.TE_MULTILINE|wx.TE_DONTWRAP)
+addon_link_textview.SetFocus()
+
+# Download button
+dlButton = wx.Button(p, wx.ID_ANY, "Download")
+dlButton.Bind(wx.EVT_BUTTON, on_start_download)
+
+# Status bar
+statusbar = wx.StatusBar(p, wx.ID_ANY, wx.STB_ELLIPSIZE_END)
+statusbar.SetStatusText("Ready to download...")
+
+vs.Add(addonslbl, 0, wx.EXPAND | wx.ALL, border=5)
+vs.Add(addons_location_field, 0, wx.EXPAND | wx.ALL, border=5)
+vs.Add(aobutton, 0, wx.EXPAND | wx.ALL, border=5)
+vs.Add(addonslinkslbl, 0, wx.EXPAND | wx.ALL, border=5)
+vs.Add(addon_link_textview, 1, wx.EXPAND | wx.ALL, border=5)
+vs.Add(dlButton, 0, wx.EXPAND | wx.ALL, border=5)
+vs.Add(statusbar, 0, wx.EXPAND)
+
+p.SetSizer(vs)
+
+frame.Show(True)
+app.MainLoop()
+
